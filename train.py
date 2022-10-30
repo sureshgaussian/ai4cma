102c1c92770fc09a323860b9950501662c787f55
rtgunti <rtgunti@gmail.com>
2022-10-30 20:54:53 +0100

    added train_loader_mini for a stable metric

--- train.py
+++ train.py
@@ -8,9 +8,7 @@ import torch.nn as nn
 import torch.optim as optim
 from unet import UNET
 from config import *
-from torchvision.models.segmentation.deeplabv3 import DeepLabHead
 from torchvision.models.segmentation import deeplabv3_resnet101
-
 
 from utils import (
     load_checkpoint,
@@ -20,14 +18,10 @@ from utils import (
     save_predictions_as_imgs,
 )
 
-# # from dice_loss import (
-# #     GDiceLossV2
-# )
-
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
 
-def train_fn(epoch_index, loader, test_loader, model, optimizer, loss_fn, scaler):
+def train_fn(epoch_index, loader, test_loader, model, optimizer, loss_fn, scaler, train_loader_mini):
     # print(f'In train function')
     # loop = tqdm(loader)
     running_loss = 0.
@@ -51,44 +45,33 @@ def train_fn(epoch_index, loader, test_loader, model, optimizer, loss_fn, scaler
         loss.backward()
         optimizer.step()
 
-        # backward
-        # print('next')
-        # scaler.scale(loss).backward()
-        # scaler.step(optimizer)
-        # scaler.update()
-
-        # update tqdm loop
-        # loop.set_postfix(loss=loss.item())
-        # Gather data and report
-        if i % 100 == 9:
+        # Print loss to console every 10th step
+        if i % 10 == 9:
             running_loss += loss.item()
             last_loss = running_loss / 10 # loss per batch
             print(f"epoch {epoch_index}  batch {i + 1}/{len(loader)} loss: {last_loss}")
-            # tb_x = epoch_index * len(loader) + i + 1
-            # tb_writer.add_scalar('Loss/train', last_loss, tb_x)
             running_loss = 0.
             
-
-        # Write to tensorboard every 1000th step
+        # Write to tensorboard every 500th step
         if i % 500 == 499:
 
             global_step_index = epoch_index*len(loader) + i
             writer.add_scalar(f"train_loss", last_loss, global_step_index)
 
-            train_dice_avg, train_dice_std, train_dice_median = check_accuracy(loader, model, device=DEVICE)
+            train_dice_avg, train_dice_std, train_dice_median = check_accuracy(train_loader_mini, model, device=DEVICE, num_batches='all')
             writer.add_scalar(f"train_dice_avg", train_dice_avg, global_step_index)
             writer.add_scalar(f"train_dice_std", train_dice_std, global_step_index)
             writer.add_scalar(f"train_dice_median", train_dice_median, global_step_index)
 
-            test_dice_avg, test_dice_std, test_dice_median = check_accuracy(test_loader, model, device=DEVICE)
+            test_dice_avg, test_dice_std, test_dice_median = check_accuracy(test_loader, model, device=DEVICE, num_batches='all')
             writer.add_scalar(f"test_dice_avg", test_dice_avg, global_step_index)
             writer.add_scalar(f"test_dice_std", test_dice_std, global_step_index)
             writer.add_scalar(f"test_dice_median", test_dice_median, global_step_index)
 
             writer.flush()
 
-        # Save checkpoint every 5k step
-        if i % 5000 == 4999:
+        # Save checkpoint every 2k step or at the last step of the epoch
+        if i % 2000 == 1999 or i == len(loader) - 1:
             CHEKPOINT_PATH_epoch_path = CHEKPOINT_PATH.replace('.pth.tar', f"_epoch_{epoch_index:02d}_step_{i:05d}.pth.tar")
             save_checkpoint(model, optimizer, CHEKPOINT_PATH_epoch_path)
             save_predictions_as_imgs(
@@ -100,7 +83,7 @@ def train_fn(epoch_index, loader, test_loader, model, optimizer, loss_fn, scaler
 
 def main(args):
 
-    POLY_TRAINING_CSV = "balanced_tiles_poly.csv"
+    POLY_TRAINING_CSV = "balanced_tiles.csv"
     TRAIN_IMG_DIR = os.path.join(TILED_INP_DIR, args.dataset+"_training/inputs")
     TRAIN_LABEL_DIR = os.path.join(TILED_INP_DIR, args.dataset+"_training/legends")
     TRAIN_MASK_DIR = os.path.join(TILED_INP_DIR, args.dataset+"_training/masks")
@@ -111,7 +94,7 @@ def main(args):
     TEST_MASK_DIR = os.path.join(TILED_INP_DIR, args.dataset+"_testing/masks")
     TEST_DESC = os.path.join(TILED_INP_DIR, args.dataset+"_testing/info/"+POLY_TRAINING_CSV)
 
-
+    MODEL_NAME = 'deeplabv3'
     if MODEL_NAME == 'unet':
         model = UNET(in_channels=IN_CHANNELS, out_channels=1).to(DEVICE)
     else:
@@ -121,14 +104,10 @@ def main(args):
     if torch.cuda.is_available():
         model.cuda()
 
-    # 
     loss_fn = nn.BCEWithLogitsLoss()
-    # loss_fn = nn.BCELoss()
-    #loss_fn = GDiceLossV2()
-    #loss_fn = DisPenalizedCE()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    train_loader, test_loader = get_loaders(
+    train_loader, test_loader, train_loader_mini = get_loaders(
         TRAIN_IMG_DIR,
         TRAIN_LABEL_DIR,
         TRAIN_MASK_DIR,
@@ -156,19 +135,13 @@ def main(args):
     scaler = torch.cuda.amp.GradScaler()
 
     for epoch_index in range(NUM_EPOCHS):
-        last_loss = train_fn(epoch_index, train_loader, test_loader, model, optimizer, loss_fn, scaler)
+        last_loss = train_fn(epoch_index, train_loader, test_loader, model, optimizer, loss_fn, scaler, train_loader_mini)
         writer.add_scalar("train_loss", last_loss, epoch_index)
 
         print(epoch_index, last_loss)
 
-        # if epoch_index % 10 == 9:
-
-        # save model
-        checkpoint = {
-            "state_dict": model.state_dict(),
-            "optimizer":optimizer.state_dict(),
-        }
-        save_checkpoint(checkpoint, checkpoint_path)
+        # save checkpoint
+        save_checkpoint(model, optimizer, checkpoint_path)
 
         # check accuracy
         print(f'Training metrics...')
@@ -176,10 +149,6 @@ def main(args):
         print(f'Testing metrics...')
         check_accuracy(test_loader, model, device=DEVICE)
 
-        # # check accuracy
-        # avg, std, median = check_accuracy(train_loader, model, device=DEVICE)
-        # avg, std, median = check_accuracy(test_loader, model, device=DEVICE)
-        # print some examples to a folder. Use train loader for dry runs
         if NUM_SAMPLES:
             save_predictions_as_imgs(
                 train_loader, model, folder=SAVED_IMAGE_PATH, device=DEVICE
