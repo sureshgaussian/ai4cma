210e16715607754a4c90a2b3d779cec03ecc0206
rtgunti <rtgunti@gmail.com>
2022-10-30 20:57:21 +0100

    Logic to expand points and lines in a mask

--- utils_show.py
+++ utils_show.py
@@ -1,22 +1,46 @@
 import numpy as np
 import cv2
 import imutils
+from PIL import Image
+
+def pil_to_opencv(img):
+    if not isinstance(img, np.ndarray):
+        return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    return img
 
 def imshow_r(wdw, img, stop=False):
     ''' Parsing cv2.imshow() '''
     if isinstance(img, list):
+        img = [pil_to_opencv(im) for im in img]
         height, width = sorted(img, key=lambda x: x.shape[0])[0].shape[:2]
         imgs = [cv2.resize(im, (width, height)) for im in img]
         img = np.hstack(tuple(imgs))
 
-    cv2.imshow(wdw, imutils.resize(img, height=800))
+    cv2.imshow(wdw, imutils.resize(img, width=800))
     
     if stop:
         cv2.waitKey()
 
 def to_rgb(image):
     ''' Convert image to RGB '''
-    return cv2.cvtColor(image.copy(), cv2.COLOR_GRAY2BGR)
+    return cv2.cvtColor(np.array(image).copy(), cv2.COLOR_GRAY2BGR)*255
 
 def to_grayscale(image):
     return cv2.cvtColor(image.copy(), cv2.COLOR_RGB2GRAY)
+
+def dilate_mask(mask, debug = False):
+    '''Extend points and lines in the masks
+       to make it easier to train segmentation networks. 
+    '''
+    if not isinstance(mask, np.ndarray):
+        mask = np.array(mask)
+    if debug:
+        imshow_r('raw_mask', mask*255, True)    
+    coords = np.argwhere(mask == 1)
+    circle_radius = 5
+    for coord in coords:
+        cv2.circle(mask, (coord[1], coord[0]), circle_radius, 1, -1)
+    if debug:
+        imshow_r('with circle', mask*255, True)
+    mask = Image.fromarray(mask)
+    return mask
